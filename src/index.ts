3f6cfda29b40155f94b81bd5196caf9828193265
bitsima <betulse42@gmail.com>
2024-02-03 18:26:35 +0300

    feat: update base point for index.ts

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import * as commander from 'commander';
+
+const program = new commander.Command();
+program.version('1.0.0');
+
+
+program
+    .command()
+
+program.parse(process.argv);
+
+
+
