4d918be85adb65daa95140522753aff0cfc8cf33
bitsima <betulse42@gmail.com>
2024-02-05 16:58:49 +0300

    fix: readWallets can now create the wallets.json, removed updateBalances, added updateBalance

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,48 +1,55 @@
-import * as fs from 'fs/promises';
-import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+import * as fs from 'fs';
+import * as fsp from 'fs/promises';
 import * as readline from 'readline';
-import chalk = require('chalk');
+import * as chalk from 'chalk';
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
 
 import { Wallet } from "./models/classes";
 import { WalletJson } from "./models/interfaces";
-
-
-
-
-
-
-
+import { WALLETS_MAP } from './index';
 
 
 /**
  * Reads the wallets.json if it exists.
- * @returns wallets array -read from the wallets.json and deserialized into Wallet instances-
+ * @returns wallets map -read from the wallets.json and deserialized into Wallet instances-
  */
-export async function readWallets(): Promise<Wallet[]> {
-    let parsedWalletData: Wallet[] = [];
+export function readWallets(): Map<string, Wallet> {
+    let walletMap: Map<string, Wallet> = null;
 
     try {
-        const walletData = await fs.readFile('./wallets.json', 'utf8');
+
+        // Synchronously check if the file exists
+        if (!fs.existsSync('./wallets.json')) {
+            // If the file doesn't exist, create an empty one
+            fs.writeFileSync('./wallets.json', '{"data": []}', 'utf-8');
+            console.log("wallets.json couldn't be found. A new one was created.")
+        }
+
+        const walletData = fs.readFileSync('./wallets.json', 'utf8');
         const jsonData: WalletJson[] = JSON.parse(walletData).data;
 
-        // Deserializing the JSON data
-        parsedWalletData = jsonData.map(walletJson => {
-            return new Wallet(
+        // Deserializing the JSON data and creating a Map
+        walletMap = new Map(jsonData.map(walletJson => [
+            walletJson.walletName,
+            new Wallet(
                 walletJson.walletName,
                 walletJson.publicKey,
                 Uint8Array.from(walletJson.secretKey),
                 walletJson.balance
-            );
-        });
+            )
+        ]));
 
     } catch (error) {
         // Handle file not found or invalid JSON
-        console.log(`Error reading or parsing wallets.json: ${error.message}\nA new wallets.json file will be created.`);
+        console.log(`Error reading or parsing wallets.json: ${error.message}`);
     }
-    return parsedWalletData;
+    return walletMap;
 }
 
-export async function writeWallets(walletObjArr: Wallet[]): Promise<void> {
+export async function writeWallets(): Promise<void> {
+
+    const walletObjArr = Array.from(WALLETS_MAP.values());
+
     // Serializing the data back to JSON
     const jsonData = {
         'data': walletObjArr.map(wallet => ({
@@ -55,34 +62,33 @@ export async function writeWallets(walletObjArr: Wallet[]): Promise<void> {
 
     // Update wallets.json with the new wallet info
     try {
-        await fs.writeFile("./wallets.json", JSON.stringify(jsonData, null, 4), 'utf8');
+        await fsp.writeFile("./wallets.json", JSON.stringify(jsonData, null, 4), 'utf8');
         console.log('wallets.json file has been updated successfully.');
     } catch (error) {
         console.error(`Error writing to JSON file: ${error.message}`);
     }
 }
 
+export async function updateBalance(walletObj: Wallet): Promise<void> {
+    const oldBalance = walletObj.balance;
 
-export async function updateBalances(): Promise<void> {
-    let wallets = await readWallets();
+    try {
+        await walletObj.checkBalance();
+    } catch (error) {
+        console.log(`Your balance on the wallet named '${walletObj.walletName}' has been updated \
+from '${oldBalance / LAMPORTS_PER_SOL}' SOL to '${walletObj.balance / LAMPORTS_PER_SOL}' SOL.`);
 
-    wallets.forEach(walletObj => {
-        const oldBalance = walletObj.balance;
-
-        try {
-            walletObj.checkBalance();
-        } catch (error) {
-            console.log(`Your balance on the wallet named '${walletObj.walletName}'\nhas been updated from '${oldBalance / LAMPORTS_PER_SOL}' SOL to '${walletObj.balance / LAMPORTS_PER_SOL}' SOL.`)
-
-        }
-    });
-
-    await writeWallets(wallets);
+        WALLETS_MAP.set(walletObj.walletName, walletObj);
+        writeWallets();
+        return;
+    }
+    console.log(`Your balance on the wallet named '${walletObj.walletName}' is the same as it's recorded in wallets.json. \
+Your balance: ${walletObj.balance / LAMPORTS_PER_SOL} SOL`);
 }
 
-export async function selectWallet(): Promise<Wallet> {
+export function selectWallet(): Promise<Wallet | null> {
 
-    let wallets = await readWallets();
+    const wallets: Wallet[] = Array.from(WALLETS_MAP.values());
 
     if (wallets.length > 0) {
         console.log(chalk.yellow('Detected existing wallets:'));
@@ -93,31 +99,32 @@ export async function selectWallet(): Promise<Wallet> {
             console.log(`[${++index}] - Wallet Name: ${wallet.walletName}, Public Key: ${wallet.publicKey}`);
         });
 
-        return new Promise((resolve, reject) => {
-            // Let the user choose which wallet to use for this instance 
+        // Let the user choose which wallet to use for this instance 
+        // Create a Promise to handle the asynchronous input
+        return new Promise((resolve) => {
             const rl = readline.createInterface({
                 input: process.stdin,
                 output: process.stdout,
             });
 
             rl.question('Please enter the order number of the wallet you want to interact with: \n', (orderNumber) => {
-                let selectedWallet = wallets[parseInt(orderNumber, 10) - 1];
+                const selectedWallet = wallets[parseInt(orderNumber, 10) - 1];
 
                 if (selectedWallet) {
                     console.log(`You selected: Wallet Name: ${selectedWallet.walletName}, Public Key: ${selectedWallet.publicKey}`);
-                    // Close the readline interface
-                    rl.close();
                     resolve(selectedWallet);
                 } else {
                     console.log('Invalid order number. Please try again.');
-                    // Close the readline interface
-                    rl.close();
-                    reject(new Error('Invalid order number'));
+                    resolve(null);
                 }
+
+                // Close the readline interface
+                rl.close();
             });
         });
     } else {
-        console.log(chalk.gray('No existing wallets detected. If you want to create one or provide it yourself, check command "new".'));
+        console.log(chalk.gray('No existing wallets detected. If you want to create one, check command "new --help".'));
+        return Promise.resolve(null);
     }
 }
 
